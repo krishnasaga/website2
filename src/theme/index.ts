ae48db1e651898045d2a717daab86c47f56e7ea3
krishnasagar <sagar474@hotmail.com>
2024-10-28 12:22:26 +0000

    add

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,4 +1,26 @@
 import { extendTheme } from "@chakra-ui/react";
 
-// Or export `extendBaseTheme` if you only want the default Chakra theme tokens to extend (no default component themes)
-export const customTheme = extendTheme({})
+export const customTheme = extendTheme({
+  colors: {
+    brand: {
+      50: "#ffe5e5",
+      100: "#fcbaba",
+      200: "#f28f8f",
+      300: "#e86565",
+      400: "#de3b3b",
+      500: "#c52222", // default brand color (red)
+      600: "#9b1b1b",
+      700: "#701414",
+      800: "#460d0d",
+      900: "#1d0505",
+    },
+  },
+
+  components: {
+    Button: {
+      defaultProps: {
+        colorScheme: "brand",
+      },
+    },
+  },
+});
